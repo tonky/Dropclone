f8bf6e72912fe741ae05a6f2d7332546960b288b
Igor Tonky <gtonky@gmail.com>
2011-12-20 13:01:31 +0200

    working upload and download with file listing

---
+++ zojax.py
@@ -0,0 +1,78 @@
+import os
+from urllib import urlencode, quote as urlquote, unquote_plus as unquote
+
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers, RequestHandler
+from google.appengine.ext.webapp import template
+
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class UpFile(db.Model):
+    name = db.StringProperty(required=True)
+    # owner = db.UserProperty()
+    uploaded = db.DateTimeProperty()
+    content = db.ByteStringProperty()
+    size = db.IntegerProperty()
+    blob_key = blobstore.BlobReferenceProperty()
+    blob_key_string = db.StringProperty()
+    ct = db.StringProperty()
+
+
+class GetFile(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        # self.send_blob(blob_info)
+        self.send_blob(blobstore.BlobInfo.get(blob_info.key()), save_as=True)
+
+
+class UploadFile(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file_upload')
+        blob_info = upload_files[0]
+
+        upfile = UpFile(name=blob_info.filename)
+
+        upfile.uploaded = blob_info.creation
+        upfile.size = blob_info.size
+        upfile.blob_key = blob_info.key()
+        upfile.blob_key_string = str(blob_info.key())
+        upfile.ct = blob_info.content_type
+
+        upfile.put()
+
+        self.redirect('/')
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
+
+        files = db.GqlQuery("SELECT * "
+                            "FROM UpFile "
+                            "ORDER BY uploaded DESC")
+
+        template_values = {
+                'files': files,
+                'upload_url': upload_url,
+                'url_linktext': "asdfsdf",
+                "nickname": False
+                }
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/upload', UploadFile),
+    ('/get/([^/]+)?', GetFile),
+
+], debug=True)
